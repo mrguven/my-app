54b797fad65b8e5271f9a302494b53fd42c6e6c6
Rauf Guven <mrguven@gmail.com>
2023-06-29 14:33:09 +0200

    update

--- src/pages/FetchFile.js
+++ src/pages/FetchFile.js
@@ -7,23 +7,34 @@ export default function FetchFile () {
     const [textList,setTextList]=useState([]);
     const [num,setNum]=useState(0);
     const [text,setText]=useState({})
-    const [isloading,setIsLoading]= useState(false)
+    const [isloading,setIsLoading]= useState(true)
 
+ const getText= ()=>{ 
+    
+        try{axios.get('https://type.fit/api/quotes').then((res)=> {setTextList(res.data);setIsLoading(false) } )} 
+        catch (err){
+          {console.log(err)}
+        }
+    
+         
+     }
+    
 
-
-  useEffect(  ()=> {axios.get('https://type.fit/api/quotes').then((res)=> setTextList(res.data) );
-  setIsLoading(true)},[])
+  useEffect( ()=> {
+    getText(); setText(setTextList[0])
+  },[])
+ 
 
 console.log('sdfgdsf');
 
 console.log(textList);
 
+console.log(text);
 
-
-
+console.log(isloading);
 
 useEffect( ()=> { 
-    setText(textList[num]); console.log(textList[num]); setIsLoading(true)
+    setText(textList[num]); console.log(textList[num]); 
 },[num] )
  
 const getNewText=()=> {
@@ -41,24 +52,27 @@ console.log(num);
 
    <h1>Quote Generator</h1>
 
-
-   {
-       
-       text
-        ?
-   <h3>
-   {text.text} <br />
-     <b>{text.author}</b>
-      
-   </h3>
-       :
-<div class="spinner-border" role="status">
-<span class="visually-hidden">Loading...</span>
-</div> 
-
+   <div>
+{
+    isloading?
+   <p></p>
+  :
+<div>
+{
+    !text ?
+  <p></p>
+    :
+    <div>
+   <i>{text.text}</i>  <br />
+   <b>{text.author}</b> 
+   </div>
+}
+   </div>
 }
 
-   
+
+
+</div>
 <button class="btn btn-success" onClick={getNewText}>new Quote</button>
 
 </div>
